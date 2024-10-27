8ee14ffd0759265ba98c71976de45f146e70a4ad
Praveen <32950689+ypraveen760@users.noreply.github.com>
2024-10-27 12:32:53 +0530

    Update constrans.js

--- src/components/utils/constrans.js
+++ src/components/utils/constrans.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDCy6re-6TE43IIISIYC98z34tampa6cN0";
+const GOOGLE_API_KEY = "..........................";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=" +
