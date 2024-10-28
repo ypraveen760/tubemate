eb9a1753b8600f820d501f422070e321c1736c81
Praveen Yadav <ypraveen760@gmail.com>
2024-10-29 01:11:43 +0530

    Search feature added

--- src/components/utils/constrans.js
+++ src/components/utils/constrans.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "..........................";
+const GOOGLE_API_KEY = "AIzaSyDCy6re-6TE43IIISIYC98z34tampa6cN0";
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
